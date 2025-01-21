9ca2c7e20d028fa1b87749666d9b1938a95ac6a9
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-01-21 12:50:28 +0000

    remove pointless comments and uncomment return

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,5 +1,3 @@
-// QuickSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -50,7 +48,7 @@ int main(int argc, char *argv[])
 
 	if (argc < 2) {
 		cout << "File not specified! Please specify a csv file to sort";
-		//return 0;
+		return 0;
 	}
 
 	string fileName = argv[1];
@@ -79,14 +77,3 @@ int main(int argc, char *argv[])
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
