d3ff6a8ca273e4c33cd3e276f544dfd51d957485
Soly <82676469+Solyphonous@users.noreply.github.com>
2025-01-21 12:49:41 +0000

    hooray! it worke

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -1,23 +1,48 @@
 // QuickSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include <fstream>
-#include <filesystem>
 #include <string>
+#include <vector>
+#include <sstream>
 
 using namespace std;
-namespace fs = std::filesystem;
 
-ifstream validateFile(const string &filename) {
-	ifstream file;
-	file.open(filename);
-	if (file) {
-		return file;
+vector<int> parse_csv(const string& input) {
+	vector<int> result;
+	stringstream ss(input);
+
+	while (ss.good()) {
+		string substr;
+		getline(ss, substr, ',');
+		result.push_back(stoi(substr));
 	}
-	else {
-		return ifstream ();
+	return result;
+}
+
+void quick_sort(vector<int> &values, int start, int end) {
+	if (start >= end) {
+		return;
 	}
+
+	int swap = start - 1;
+	const int pivot_index = end;
+
+	for (int i = start; i <= end; i++) {
+		if (values[i] <= values[pivot_index]) {
+			swap++;
+
+			if (i > swap) {
+				int temp = values[swap];
+				values[swap] = values[i];
+				values[i] = temp;
+			}
+		}
+	}
+
+	quick_sort(values, start, swap - 1);
+	quick_sort(values, swap + 1, end);
+
 }
 
 int main(int argc, char *argv[])
@@ -25,12 +50,11 @@ int main(int argc, char *argv[])
 
 	if (argc < 2) {
 		cout << "File not specified! Please specify a csv file to sort";
-		return 0;
+		//return 0;
 	}
 
 	string fileName = argv[1];
-	ifstream file;
-	file.open(fileName);
+	ifstream file(fileName);
 
 	if (!file) {
 		cout << "File does not exist!";
@@ -38,19 +62,20 @@ int main(int argc, char *argv[])
 	}
 
 	if ( !(fileName.substr(fileName.find_last_of(".") + 1) == "csv") ) {
-		cout << "File is not csv!";
+		cout << "File is not a csv!";
 		return 0;
 	}
 
-	//fs::path cwd = fs::current_path();
-	//cout << cwd;
+	string contents;
+	file >> contents;
 
-	//file.open("bingozing.txt", ios::out);
+	vector<int> values = parse_csv(contents);
 
-	//if (file.is_open()) {
-	//	file << "yo";
-	//	file.close();
-	//}
+	quick_sort(values, 0, values.size()-1);
+
+	for (int i = 0; i < values.size(); i++) {
+		cout << values[i] << ',';
+	}
 
 	return 0;
 }
@@ -63,5 +88,5 @@ int main(int argc, char *argv[])
 //   2. Use the Team Explorer window to connect to source control
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
